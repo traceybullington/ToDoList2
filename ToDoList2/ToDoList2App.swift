463be79f60ba1380db90e70c98c6c64ea1c0da7f
Tracey Bullington <traceylbullington@gmail.com>
2023-05-20 16:08:35 -0500

    Initial Commit

---
+++ ToDoList2/ToDoList2App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList2App.swift
+//  ToDoList2
+//
+//  Created by scholar on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
